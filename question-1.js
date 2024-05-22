5c6775521932e9d3b6183ea4fd526b90bbd196bc
revieww12 <review120244@gmail.com>
2024-05-22 15:52:23 +0700

    feat: add code js-skill-checkpoint-part-1

--- question-1.js
+++ question-1.js
@@ -6,3 +6,18 @@ const myTodo = [
   { id: 4, todo: "Wash dishes" },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+
+// 1) เพิ่ม To-do ใหม่ชื่อว่า "Walk the dog"
+myTodo.push({ id: 5, todo: "Walk the dog" });
+console.log(myTodo);
+
+// 2) ให้แก้ไขชื่อ Todo ที่มี id เป็น 4 จาก "Wash dishes" เป็น "Go to the gym"
+myTodo[3].todo = "Go to the gym";
+console.log(myTodo);
+
+// 3) ลบ To-do สุดท้ายที่อยู่ใน Array ออก
+myTodo.pop();
+console.log(myTodo);
+
+// 4) ให้แสดงข้อความของ To-do ตัวสุดท้ายที่อยู่ในรายการออกมาบนหน้าจอ Console
+console.log(`To-do id: ${myTodo[3].id}, ${myTodo[3].todo}`);
--- question-2.js
+++ question-2.js
@@ -4,3 +4,19 @@ const inventory = [
   { name: "Banana", price: 10, quantity: 50 },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+
+// 1) ให้แก้ไขจำนวนสินค้า “Apple” จาก 100 เป็น 200
+inventory[0].quantity = 200;
+let appleQuantity = inventory[0].quantity;
+console.log(appleQuantity);
+
+// 2) เพิ่มสินค้าใหม่ที่ชื่อ “Orange” ที่มีราคา 20 บาท และมีจำนวน 300 ชิ้นในสต็อก
+inventory.push({ name: "Orange", price: 20, quantity: 300 });
+console.log(inventory);
+
+// 3) ให้คำนวณมูลค่ารวมของจำนวนสินค้าทั้งหมดในสต็อก
+let totalApple = inventory[0].quantity * inventory[0].price;
+let totalBanana = inventory[1].quantity * inventory[1].price;
+let totalOrange = inventory[2].quantity * inventory[2].price;
+let totalAll = totalApple + totalBanana + totalOrange;
+console.log(`มูลค่ารวมของจำนวนสินค้าทั้งหมดในสต็อก ${totalAll} บาท`);
